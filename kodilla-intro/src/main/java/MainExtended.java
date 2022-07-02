9a6ab4942b109506ad24433a9d11ecc153fdf32e
Tomasz Raczek <tomaszrczek666@gmail.com>
2022-07-02 08:44:30 +0200

    Main Extended 0.8

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,20 @@
+public class MainExtended  {
+    public static void main(String[] args) {
+        String recipeName = "Chef Badgers' Pancakes";
+        int recipeYield = 4;
+        char recipeClass = 'A';
+        String welcome = "Welcome to Cooking Class!";
+        String ingridientsNeeded = "To make this recipe you will need the best quality ingridients. Here is the list:";
+        String recipeConclusion = "I hope you had fun making this recipe. Be sure to check out my other stuff";
+        System.out.println(welcome);
+        System.out.print("Today, we will learn how to make ");
+        System.out.println(recipeName);
+        System.out.print("This is truly an ");
+        System.out.print(recipeClass);
+        System.out.print(" class recipe. The recipe makes ");
+        System.out.print(recipeYield);
+        System.out.println(" pancakes");
+        System.out.println(ingridientsNeeded);
+        System.out.println("INGRIDIENTS AND RECIPE HERE");
+        System.out.print(recipeConclusion);}
+}
