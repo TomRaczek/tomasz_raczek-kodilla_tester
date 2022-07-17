8e52f0cfa69bb2ac79a243f0e1db5ed1173cded5
Tomasz Raczek <tomaszrczek666@gmail.com>
2022-07-17 09:46:52 +0200

    Task 2.6

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,14 @@
+public class Man {
+
+    String name;
+    double age;
+    double height;
+
+    public Man(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        
+    }
+}
+
