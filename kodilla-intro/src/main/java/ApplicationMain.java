cbdd86a4d546f9972c049028e98ddc3e84aedf11
Tomasz Raczek <tomaszrczek666@gmail.com>
2022-07-17 09:49:19 +0200

    Task 2.6 corrected

--- kodilla-intro/src/main/java/ApplicationMain.java
+++ kodilla-intro/src/main/java/ApplicationMain.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class ApplicationMain {
+public class ApplicationMain {
+    public static void main(String[] args) {
+
+        Application Adam = new Application("Adam", 40.5, 178);
+
+        System.out.println(Adam.getApplicationInfo());
+    }
 }
